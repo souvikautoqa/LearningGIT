7bb4bdab10c57a53459c33cc83c6ad88bf176dec
duttas <souvik.dutta@hmhco.com>
2020-02-22 10:54:17 +0000

    Learning GIT commit -2

--- LearningGIT.java
+++ LearningGIT.java
@@ -4,5 +4,6 @@ public class LearningGIT {
     public static void main(String[] args) {
         System.out.println("Learning GIT");
         System.out.println("Learning GIT adding more feature");
+        System.out.println("Learning GIT adding more feature for next commit");
     }
 }
